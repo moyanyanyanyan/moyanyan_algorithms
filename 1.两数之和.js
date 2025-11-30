23a24e53d7b9d45e026c04ede7c083ae0ea58aa3
moyanyanyanyan <804075270@qq.com>
2025-11-30 22:01:50 +0800

    加了注释

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -1,12 +1,15 @@
 var twoSum = function (nums, target) {
-  let sum = 0
-  let res = []
+  let sum = 0 // 记录两数之和
+  let res = [] // 记录结果
   for (let i = 0; i < nums.length; i++) {
+    // 遍历数组
     for (let j = i + 1; j < nums.length; j++) {
-      sum = nums[i] + nums[j]
+      // 从i+1开始遍历，避免重复计算
+      sum = nums[i] + nums[j] // 计算两数之和
       if (sum == target) {
-        res[0] = i
-        res[1] = j
+        // 如果两数之和等于目标值
+        res[0] = i // 记录第一个数的索引
+        res[1] = j // 记录第二个数的索引
       }
     }
   }
--- 344.反转字符串.js
+++ 344.反转字符串.js
@@ -1,14 +1,15 @@
 var reverseString = function (s) {
   //延用双指针
-  let left = 0
-  let right = s.length - 1
+  let left = 0 //左指针
+  let right = s.length - 1 //右指针
   let tem = 0
   while (left < right) {
-    tem = s[left]
+    //当左指针小于右指针时
+    tem = s[left] //交换
     s[left] = s[right]
     s[right] = tem
-    left++
-    right--
+    left++ //左指针右移
+    right-- //右指针左移
   }
   return s
 }
