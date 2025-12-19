71476c9a8a7d42298b6c01c18fa321fbd7c0f5b4
moyanyanyanyan <804075270@qq.com>
2025-12-19 22:37:37 +0800

    数组交集set做法

--- 349.两个数组的交集.js
+++ 349.两个数组的交集.js
@@ -25,4 +25,24 @@ var intersection = function (nums1, nums2) {
   }
   return same
 }
-//三个循环，我没招了
+//三个循环，我没招了，时间复杂度最大为O(n × m × min(n, m))，这个是我当时拿循环硬着头皮搞的
+//以下是set的解决方案
+var intersection = function (nums1, nums2) {
+  let nums1Set = new Set(nums1) //set自动去重，减少循环工作量
+  let nums2Set = new Set(nums2)
+  let same = new Set()
+  if (nums2Set.size > nums1Set.size) {
+    //这里使得nums1Set总是size小的哪一个，避免我在if else
+    let tem = nums1Set //语句里面复制粘贴的时候有的东西忘记改，我下面的遍历只要写一遍就够了
+    nums1Set = nums2Set
+    nums2Set = tem
+  }
+  for (let num of nums2Set) {
+    //遍历小的set
+    if (nums1Set.has(num)) {
+      //用了方法has，这个方法的时间复杂度为O（1），底层是哈希表
+      same.add(num) //实际上的性能比我再写一个for去遍历nums1Set好，再 写for时间复杂度就变成O（n）了
+    }
+  }
+  return Array.from(same)
+}
